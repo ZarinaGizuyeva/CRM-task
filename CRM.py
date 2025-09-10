d66b66b5971baf4fb92f3b01d547824018daf308
Zarina <gizueva.zarina@gmail.com>
2025-09-10 16:24:37 +0500

    add enums

--- CRM.py
+++ CRM.py
@@ -1,10 +1,46 @@
-import random
-class Subject:
-    subjects = dict() #здесь словарь, где ключ это предмет, а значение учитель
-    def __init__(self, subject_name):
-        self.subject_name = subject_name
+import enum
 
-class Users:
+
+class Subject(enum.StrEnum):
+    MATHS = 'maths'
+    HISTORY = 'history'
+    ECONOMICS = 'economics'
+    ENGLISH = 'english'
+    PSYCHOLOGY = 'psychology'
+    INFORMATICS = 'informatics'
+
+    def __repr__(self):
+        return self.value
+
+
+class Mark(enum.IntEnum):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+
+    def __repr__(self):
+        return str(self.value)
+
+    def __str__(self):
+        return self.value
+
+
+class Week(enum.Enum):
+    MONDAY = 'Monday'
+    TUESDAY = 'Tuesday'
+    WEDNESDAY = 'Wednesday'
+    THURSDAY = 'Thursday'
+    FRIDAY = 'Friday'
+    SATURDAY = 'Saturday'
+    SUNDAY = 'Sunday'
+
+    def __repr__(self):
+        return self.name
+
+
+class User:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
@@ -15,48 +51,61 @@ class Users:
     def __str__(self):
         return f"{self.name} {self.surname}"
 
-class Student(Users):
+    @staticmethod
+    def get_schedule(day: Week):
+        match day:
+            case Week.MONDAY:
+                return Subject.MATHS, Subject.ENGLISH
+            case Week.TUESDAY:
+                return Subject.HISTORY, Subject.ECONOMICS
+            case Week.WEDNESDAY:
+                return Subject.INFORMATICS
+            case Week.THURSDAY:
+                return Subject.PSYCHOLOGY
+            case Week.FRIDAY:
+                return Subject.INFORMATICS
+            case _:
+                return tuple()
 
-    list_of_students = [] #список всех учеников
-    students_marks = dict() #словарь с оценками учеников, где ключ ученик, значения список оценок
-    def add_students(self):
-        self.list_of_students.append(self)
 
-    def do_homework(self, subject_name):
-        if subject_name in Subject.subjects:
-            print(f'{self} делает домашку по {Subject.subjects[subject_name]}')
-        else: print(f'{self} не изучает такой предмет')
+class Student(User):
+    students_marks = dict()  # словарь с оценками учеников, где ключ ученик, значения список оценок
 
-    def check_marks(self, subject_name):
-        if subject_name in Subject.subjects:
-            print(f'{self} получил оценку по {Subject.subjects[subject_name]}')
-        else: print(f'{self} не изучает такой предмет')
+    def check_marks(self):
+        if self in self.students_marks:
+            return self.students_marks[self]
+        return self.students_marks
 
-    def take_exam(self):
-        print(f'{self} сдает экзамен у преподавателя {Teacher1}')
+    def do_homework(self, subject: Subject):
+        return f'{self} делает домашку по {subject}'
 
-class Teacher(Users):
-    def teach(self, subject_name):
-        Subject.subjects.setdefault(subject_name, self.surname) #добавляю предметы и преподавателей в словарь
-        print(f'{self} преподает {subject_name}')
-        print(Subject.subjects)
 
-    def give_marks(self): #ставим оценки ученикам
-        for student in Student.list_of_students:
-            if student not in Student.students_marks:
-                Student.students_marks[student] = [random.choice([1, 2, 3, 4, 5])]
-            elif student in Student.students_marks:
-                Student.students_marks[student].append(random.choice([1, 2, 3, 4, 5]))
-        print(Student.students_marks)
+class Teacher(User):
+    teachers = dict()  # словарь с учителями и предметами
+
+    def teach(self, subject: Subject):
+        self.teachers.setdefault(subject, self)
+
+    @classmethod
+    def give_marks(cls, student: Student, mark: Mark):  # ставим оценки ученикам
+        if student not in Student.students_marks:
+            Student.students_marks[student] = [mark]
+        Student.students_marks[student].append(mark)
+
 
 Student1 = Student('Sasha', 'Snigirev')
 Student2 = Student('Kolya', 'Pak')
 Teacher1 = Teacher('Pavel', 'Stepanov')
-Teacher1.teach('Maths')
-Student1.add_students()
-Student2.add_students()
-Teacher1.give_marks()
+Teacher1.teach(Subject.HISTORY)
+Teacher1.give_marks(Student1, Mark.ONE)
 Teacher2 = Teacher('Anton', 'Kashin')
-Teacher2.teach('History')
-Teacher2.give_marks()
-Student1.do_homework('English')
+Teacher2.teach(Subject.ECONOMICS)
+Teacher2.give_marks(Student2, Mark.THREE)
+Teacher2.give_marks(Student1, Mark.FOUR)
+Student1.check_marks()
+Student1.get_schedule(Week.MONDAY)
+Student3 = Student('Tolik', 'Losev')
+Student3.check_marks()
+Student1.get_schedule(Week.SUNDAY)
+print(Teacher.teachers)
+print(Student.students_marks)
